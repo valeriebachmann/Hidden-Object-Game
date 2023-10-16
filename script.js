43506b0f77135096d772017baa4bfc2d6c7c3ef6
Valerie Bachmann <valibachmann@gmail.com>
2023-10-16 16:12:21 +0200

    add total score

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function hidePopup() {
 function incrementScore(popupId) {
     if (!clickedIds.includes(popupId)) {
         score += 1;
-        document.getElementById("scoreText").innerHTML = "Score: " + score;
+        document.getElementById("scoreText").innerHTML = "Score: " + score + " / 10";
         clickedIds.push(popupId);
     }
 }
@@ -48,11 +48,11 @@ const popUps = {
     physical: {
         imageUrl: "img/#.png",
         title: "Körperbehinderung",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae incidunt fuga iste, error nemo libero accusamus ratione sapiente nisi quisquam! Esse excepturi, officiis amet, ut aspernatur eius est distinctio fuga deleniti dolor quaerat voluptatum quidem? Eos, eligendi officiis explicabo temporibus esse hic voluptates corporis aperiam similique deleniti soluta eum tenetur quibusdam voluptate pariatur ea. Aspernatur optio quo pariatur nisi fugiat unde harum inventore. Repudiandae esse impedit quisquam dignissimos omnis, eligendi sed, neque aperiam, ullam iure consequuntur officia aliquid id. Laudantium, voluptatum! Dolorum facilis blanditiis quia eum, aperiam ut quasi repellat possimus reprehenderit sequi perferendis temporibus! Provident illum, odio, necessitatibus in incidunt nisi dignissimos suscipit beatae minima eius dolor tenetur veniam praesentium eos saepe enim ex quaerat libero porro laboriosam consequuntur iste, aspernatur cumque dolorem. Iste illum eveniet error similique pariatur ipsa dignissimos aspernatur ducimus commodi sint, reiciendis quisquam omnis aliquam rem expedita voluptate id autem. Corporis reprehenderit, blanditiis quibusdam dolor facilis itaque quod fuga earum saepe quo unde nobis facere quaerat, molestias praesentium quis accusamus debitis culpa quae laboriosam dolores incidunt quam libero! Eum pariatur molestias omnis voluptates quas error obcaecati ad natus iusto id, quae quidem corporis, dolor quibusdam minus incidunt illo, ullam iste nam! Suscipit labore inventore ut."
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae incidunt fuga iste, error nemo libero accusamus ratione sapiente nisi quisquam! Esse excepturi, officiis amet."
     },
     racism: {
         imageUrl: "img/#.png",
         title: "Rassismus",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae incidunt fuga iste, error nemo libero accusamus ratione sapiente nisi quisquam! Esse excepturi, officiis amet, ut aspernatur eius est distinctio fuga deleniti dolor quaerat voluptatum quidem? Eos, eligendi officiis explicabo temporibus esse hic voluptates corporis aperiam similique deleniti soluta eum tenetur quibusdam voluptate pariatur ea. Aspernatur optio quo pariatur nisi fugiat unde harum inventore. Repudiandae esse impedit quisquam dignissimos omnis, eligendi sed, neque aperiam, ullam iure consequuntur officia aliquid id. Laudantium, voluptatum!"
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae incidunt fuga iste, error nemo libero accusamus ratione sapiente nisi quisquam! Esse excepturi, officiis amet, ut aspernatur eius est distinctio fuga deleniti dolor quaerat voluptatum quidem!"
     }
 }
