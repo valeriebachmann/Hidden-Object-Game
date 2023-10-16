f9d900d032763a1c6c7df8e6b4e35f075b802b7e
Valerie Bachmann <valibachmann@gmail.com>
2023-10-16 16:00:29 +0200

    add progressbar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const popUp = document.getElementById("popUp");
+const progress = document.querySelector(".progressBarWithScore");
 let score = 0;
 let clicked = false;
 const clickedIds = [];
@@ -6,8 +7,8 @@ const clickedIds = [];
 function onObjectClick(popupId) {
     showPopup(popupId);
     incrementScore(popupId);
+    incrementProgressBarWidth();
 }
-
 
 //Füllt den PopUp mit Inhalt ab 
 function showPopup(popupId) {
@@ -30,20 +31,26 @@ function incrementScore(popupId) {
     }
 }
 
+// 10: Beispiel max. 10 Objekte zu finden
+function incrementProgressBarWidth() {
+    progress.style.width = `${(score / 10) * 100}%`;
+    progress.innerText = `${Math.ceil((score / 10) * 100)}%`;
+}
+
 //Inhalt für die PopUps (Beispiele)
 // 1. Bild  2. Titel  3. Text
 const popUps = {
-    stehpult: {
+    visual: {
         imageUrl: "img/#.png",
         title: "Sehbehinderung",
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae incidunt fuga iste, error nemo libero accusamus ratione sapiente nisi quisquam! Esse excepturi, officiis amet, ut aspernatur eius est distinctio fuga deleniti dolor quaerat voluptatum quidem? Eos, eligendi officiis explicabo temporibus esse hic voluptates corporis aperiam similique deleniti soluta eum tenetur quibusdam voluptate pariatur ea. Aspernatur optio quo pariatur nisi fugiat unde harum inventore. Repudiandae esse impedit quisquam dignissimos omnis, eligendi sed, neque aperiam, ullam iure consequuntur officia aliquid id. Laudantium, voluptatum! Dolorum facilis blanditiis quia eum, aperiam ut quasi repellat possimus reprehenderit sequi perferendis temporibus! Provident illum, odio, necessitatibus in incidunt nisi dignissimos suscipit beatae minima eius dolor tenetur veniam praesentium eos saepe enim ex quaerat libero porro laboriosam consequuntur iste, aspernatur cumque dolorem. Iste illum eveniet error similique pariatur ipsa dignissimos aspernatur ducimus commodi sint, reiciendis quisquam omnis aliquam rem expedita voluptate id autem!"
     },
-    whiteboardWischer: {
+    physical: {
         imageUrl: "img/#.png",
         title: "Körperbehinderung",
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae incidunt fuga iste, error nemo libero accusamus ratione sapiente nisi quisquam! Esse excepturi, officiis amet, ut aspernatur eius est distinctio fuga deleniti dolor quaerat voluptatum quidem? Eos, eligendi officiis explicabo temporibus esse hic voluptates corporis aperiam similique deleniti soluta eum tenetur quibusdam voluptate pariatur ea. Aspernatur optio quo pariatur nisi fugiat unde harum inventore. Repudiandae esse impedit quisquam dignissimos omnis, eligendi sed, neque aperiam, ullam iure consequuntur officia aliquid id. Laudantium, voluptatum! Dolorum facilis blanditiis quia eum, aperiam ut quasi repellat possimus reprehenderit sequi perferendis temporibus! Provident illum, odio, necessitatibus in incidunt nisi dignissimos suscipit beatae minima eius dolor tenetur veniam praesentium eos saepe enim ex quaerat libero porro laboriosam consequuntur iste, aspernatur cumque dolorem. Iste illum eveniet error similique pariatur ipsa dignissimos aspernatur ducimus commodi sint, reiciendis quisquam omnis aliquam rem expedita voluptate id autem. Corporis reprehenderit, blanditiis quibusdam dolor facilis itaque quod fuga earum saepe quo unde nobis facere quaerat, molestias praesentium quis accusamus debitis culpa quae laboriosam dolores incidunt quam libero! Eum pariatur molestias omnis voluptates quas error obcaecati ad natus iusto id, quae quidem corporis, dolor quibusdam minus incidunt illo, ullam iste nam! Suscipit labore inventore ut."
     },
-    Treppe: {
+    racism: {
         imageUrl: "img/#.png",
         title: "Rassismus",
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae incidunt fuga iste, error nemo libero accusamus ratione sapiente nisi quisquam! Esse excepturi, officiis amet, ut aspernatur eius est distinctio fuga deleniti dolor quaerat voluptatum quidem? Eos, eligendi officiis explicabo temporibus esse hic voluptates corporis aperiam similique deleniti soluta eum tenetur quibusdam voluptate pariatur ea. Aspernatur optio quo pariatur nisi fugiat unde harum inventore. Repudiandae esse impedit quisquam dignissimos omnis, eligendi sed, neque aperiam, ullam iure consequuntur officia aliquid id. Laudantium, voluptatum!"
