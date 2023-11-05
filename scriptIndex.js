abea7a87e18bc4e0fa1f6fbdd184d7758943b060
Valerie Bachmann <valibachmann@gmail.com>
2023-11-05 18:58:18 +0100

    add index animation

--- scriptIndex.js
+++ scriptIndex.js
@@ -19,3 +19,27 @@ function test() {
     console.log("test");
 }
 
+function animateHomeScreen() {
+    const animationConfig = { duration: 300, fill: "forwards" }
+    const gameTitle = document.getElementById("gameTitle");
+    const blueButtonGroup = document.getElementById("homeScreenBlueButtonGroup");
+    const startGameButtonText = document.getElementById("startGameButtonText");
+    const infoGebaeude = document.getElementById("infoGebaeude");
+    const image1 = document.getElementById("image1");
+    const indexButton = document.getElementById("indexButton")
+
+
+    startGameButtonText.animate([{ transform: "translateY(-100vh)" }], animationConfig)
+    gameTitle.animate([{ opacity: 0 }], animationConfig)
+    blueButtonGroup.animate([{
+        transform: "translateY(100vh)"
+    }], animationConfig)
+    infoGebaeude.animate([{ opacity: 1 }], animationConfig)
+    image1.animate([{ opacity: 0 }], animationConfig)
+    setTimeout(() => {
+        indexButton.style.display = "none"
+        gameTitle.style.display = "none"
+        image1.style.display = "none"
+    }, animationConfig.duration);
+
+}
